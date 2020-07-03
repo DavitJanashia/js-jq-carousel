7d983eefd912ea96588559ac75a834ed85ab9444
Davit <davit.janashia1@gmail.com>
2020-07-03 19:47:20 +0200

    fatto frecce e pallini (mancano i bottoni)

---
+++ main.js
@@ -0,0 +1,68 @@
+//***************************************************************
+//prova 2
+
+$(document).ready(
+  function (){
+    var next = $('.fas.fa-angle-right');
+    next.click(
+      function (){
+        var myActive = $('.my-img-box.active');
+        var myFirstActive = $('.my-img-box.first');
+        var myLastActive = $('.my-img-box.last.active');
+        var myHasClass = $(".my-img-box.active").hasClass("active");
+        var myHasClass2 = $(".my-img-box.active").hasClass('last');
+
+        var pallinoBlu = $('.fas.fa-circle.active');
+        var pF = $('.fas.fa-circle.first');
+        var pLA = $('.fas.fa-circle.last.active');
+
+        if(myHasClass == true && myHasClass2 != true){
+          myActive.removeClass('active');
+          myActive.next().addClass('active');
+
+          pallinoBlu.removeClass('active');
+          pallinoBlu.next().addClass('active');
+        }
+         else if(myHasClass2 == true){
+          myLastActive.removeClass('active');
+          myFirstActive.addClass('active');
+
+          pLA.removeClass('active');
+          pF.addClass('active');
+        }
+      }
+    );
+    
+    var prev = $('.fas.fa-angle-left');
+    prev.click(
+      function (){
+        var myActive = $('.my-img-box.active');
+        var myFirstActive = $('.my-img-box.first.active');
+        var myLast = $('.my-img-box.last');
+        var myHasClass = $(".my-img-box.active").hasClass("active");
+        var myHasClass1 = $(".my-img-box.active").hasClass('first');
+
+        var pallinoBlu = $('.fas.fa-circle.active');
+        var pFA = $('.fas.fa-circle.first.active');
+        var pL = $('.fas.fa-circle.last');
+
+
+        if(myHasClass == true && myHasClass1 != true){
+          myActive.removeClass('active');
+          myActive.prev().addClass('active');
+
+          pallinoBlu.removeClass('active');
+          pallinoBlu.prev().addClass('active');
+        }
+         else if(myHasClass1 == true){
+          myFirstActive.removeClass('active');
+          myLast.addClass('active');
+
+          pFA.removeClass('active');
+          pL.addClass('active');
+        }
+      }
+    );
+
+  }
+);
